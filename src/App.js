b6652fa0edc6b250bb1bbba70b14a680e156d17a
Emmanuel Joseph(JET) <emmanueljet774@gmail.com>
2021-02-23 17:40:41 +0100

    Initial Frontend Release

--- src/App.js
+++ src/App.js
@@ -1,22 +1,15 @@
-import logo from './logo.svg';
+import conceptImage from './assets/images/treate_concept.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={conceptImage} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Technological Creations (<code>Treate</code>)<br></br>
+          <small>Connect. Coach. Create.</small>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
